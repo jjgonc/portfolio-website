300aa3072658374e7256e7000e19cc0d917ba056
jjgonc <josejoao_2001@hotmail.com>
2023-10-17 16:39:29 +0100

    main website done, just needs some fixes and update skills and experience

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,6 +8,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Education from './components/Education';
+import Footer from './components/Footer';
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -33,9 +34,11 @@ function App() {
             <Experience/>
           </Wrapper>
           <Projects/>
-          <Wrapper>
+          {/* <Wrapper>
             <Education/>
-          </Wrapper>
+            <Contact/>
+          </Wrapper> */}
+          <Footer/>
         </Body>
       </Router>
     </ThemeProvider>
