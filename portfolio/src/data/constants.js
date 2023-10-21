648885502cdbf8cf77478477ee188f4d0c83c44c
jjgonc <josejoao_2001@hotmail.com>
2023-10-21 12:29:42 +0100

    changed page name

--- portfolio/src/data/constants.js
+++ portfolio/src/data/constants.js
@@ -12,7 +12,7 @@ import f3m from "../images/f3m.png"
 export const Bio = {
     name: "José Gonçalves",
     roles: [
-      "Software Developer",
+      "Software Engineer",
       "Masters Student",
     ],
     description: "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
